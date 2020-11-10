5b993a868d5422886989ec0c01bd9986ae687c26
dylanvaneijk <2114448@talnet.nl>
2020-11-10 21:05:40 +0100

    .

---
+++ 05 - Javascript/taak07 - Intelligente DOM/website/script.js taak08 - Document Object Model.js
@@ -0,0 +1,13 @@
+
+console.log(window.document);
+
+
+
+
+
+ var elem = document.querySelector("#poster1");{elem.style.backgroundColor = '#e5e5e5';}
+ 
+
+
+var elem = document.querySelector("#poster2");{elem.style.backgroundColor = '#FF0000';}
+ var elem = document.querySelector("#poster3");{elem.style.backgroundColor='#0000FF';}
